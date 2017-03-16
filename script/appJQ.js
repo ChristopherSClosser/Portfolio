79c3716229c3508bb14edc35a995d5b573d66467
Chris Closser <chrisclosser1@gmail.com>
2017-03-16 11:56:33 -0700

    added projects constructor

--- script/appJQ.js
+++ script/appJQ.js
@@ -1,9 +1,18 @@
 'use strict';
 
 $('.burger').click(function () {
-  if ( $( 'nav' ).is( ':hidden' ) ) {
-    $( '#main-nav' ).slideDown( 'slow' );
+  if ( $('nav').is(':hidden') ) {
+    $('#main-nav').slideDown('slow');
   } else {
-    $( '#main-nav' ).slideUp();
+    $('#main-nav').slideUp('fast');
   }
 });
+
+var x = $('#main-nav'); // this is your element
+var w = $(window).width();
+if(w >= 500) {
+  console.log('the window is larger than ', w);
+  x.removeClass('hidden');
+}else{
+  x.addClass('hidden');
+}
