63d963ba76c70e148d652163d61224a1b7d823ba
Chris Closser <chrisclosser1@gmail.com>
2017-03-16 12:04:02 -0700

    added .ready function

--- script/appJQ.js
+++ script/appJQ.js
@@ -1,18 +1,19 @@
 'use strict';
 
-$('.burger').click(function () {
-  if ( $('nav').is(':hidden') ) {
-    $('#main-nav').slideDown('slow');
-  } else {
-    $('#main-nav').slideUp('fast');
+$(document).ready(function(){
+  $('.burger').on('click', function () {
+    if ( $('nav').is(':hidden') ) {
+      $('#main-nav').slideDown('slow');
+    } else {
+      $('#main-nav').slideUp('fast');
+    }
+  });
+
+  var w = $(window).width();
+  if(w >= 500) {
+    console.log('the window is larger than ', w);
+    x.removeClass('hidden');
+  }else{
+    x.addClass('hidden');
   }
 });
-
-var x = $('#main-nav'); // this is your element
-var w = $(window).width();
-if(w >= 500) {
-  console.log('the window is larger than ', w);
-  x.removeClass('hidden');
-}else{
-  x.addClass('hidden');
-}
