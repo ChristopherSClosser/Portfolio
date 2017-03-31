5eb63cc44ae4d1021fc24745ba2a8db147b4a597
Chris Closser <chrisclosser1@gmail.com>
2017-03-31 13:48:02 -0700

    lost github

---
+++ script/views/repoview.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function(module) {
+  const repoView = {};
+
+  const ui = function() {
+    let $about = $('#about');
+
+    $about.find('ul').empty();
+    $about.show().siblings().hide();
+  };
+
+  repoView.index = function() {
+    ui();
+    let source = $('#repo-template').html();
+    let templateRender = Handlebars.compile(source);
+
+    // The jQuery `append` method lets us append an entire array of HTML elements at once:
+    $('.github').append(
+      repos.with('name').map(templateRender) // Want to filter by a different property other than name?
+    );
+  };
+
+  module.repoView = repoView;
+})(window);
