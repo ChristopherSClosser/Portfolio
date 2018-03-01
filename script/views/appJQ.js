830f1cba7516121f18ade062d9966b46be5a78d6
Chris Closser <chrisclosser1@gmail.com>
2018-03-01 14:48:44 -0800

    mobile address bar hide 4

--- script/views/appJQ.js
+++ script/views/appJQ.js
@@ -15,6 +15,20 @@ $(function(){
   $('#home, #github, #gallery, #me').on('click', function(){
     if (window.innerWidth <= 739){
       $('#main-nav').slideUp('fast');
+      function toggleFullScreen() {
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          requestFullScreen.call(docEl);
+        }
+        else {
+          cancelFullScreen.call(doc);
+        }
+      }
     }
   });
 
