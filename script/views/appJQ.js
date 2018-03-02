7f29e945affb982d84253391933fb26cac60210e
Chris Closser <chrisclosser1@gmail.com>
2018-03-01 17:24:05 -0800

    hide nav on scroll down - show on scroll up

--- script/views/appJQ.js
+++ script/views/appJQ.js
@@ -1,5 +1,31 @@
 'use strict';
 
+$(function(){
+  //Keep track of last scroll
+  var lastScroll = 0;
+  $(window).scroll(function(event){
+    //Sets the current scroll position
+    var st = $(this).scrollTop();
+    //Determines up-or-down scrolling
+    if (st > lastScroll){
+      //Replace this with your function call for downward-scrolling
+      // console.log('scrolling down');
+      $('.ckata').hide();
+      $('.linked').hide();
+      $('header').removeClass('nav-down').addClass('nav-up');
+    }
+    else {
+      //Replace this with your function call for upward-scrolling
+      // console.log('scrolling up');
+      $('header').removeClass('nav-up').addClass('nav-down');
+      $('.ckata').fadeIn('fast');
+      $('.linked').fadeIn('fast');
+    }
+    //Updates scroll position
+    lastScroll = st;
+  });
+});
+
 //function for mobile menu on page ready
 $(function(){
   $('section').hide();
@@ -15,20 +41,20 @@ $(function(){
   $('#home, #github, #gallery, #me').on('click', function(){
     if (window.innerWidth <= 739){
       $('#main-nav').slideUp('fast');
-      function toggleFullScreen() {
-        var doc = window.document;
-        var docEl = doc.documentElement;
-
-        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-          requestFullScreen.call(docEl);
-        }
-        else {
-          cancelFullScreen.call(doc);
-        }
-      }
+      // function toggleFullScreen() {
+      //   var doc = window.document;
+      //   var docEl = doc.documentElement;
+      //
+      //   var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+      //   var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+      //
+      //   if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+      //     requestFullScreen.call(docEl);
+      //   }
+      //   else {
+      //     cancelFullScreen.call(doc);
+      //   }
+      // }
     }
   });
 
